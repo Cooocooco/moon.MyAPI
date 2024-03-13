173ae3c8e92b2d057885188da50e69a93bc4618e
Yang <483867474@qq.com>
2024-03-13 13:19:47 +0800

    优化体验

--- moon.MyAPI/Models/DataModel.cs
+++ moon.MyAPI/Models/DataModel.cs
@@ -19,23 +19,4 @@ namespace moon.MyAPI.Models
         /// </summary>
         public string Date { get; set; }
     }
-
-
-    public class S7DataModel
-    {
-        /// <summary>
-        /// 设备IP地址
-        /// </summary>
-        public string Ip { get; set; }
-
-        /// <summary>
-        /// 设备数据字典，用于存放读取到的所有数据
-        /// </summary>
-        public Dictionary<string, byte[]> Data { get; set; }
-
-        /// <summary>
-        /// 读取数据的时间戳
-        /// </summary>
-        public string Date { get; set; }
-    }
 }
